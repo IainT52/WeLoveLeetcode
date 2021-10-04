bb4fac1b168be6e444474b157242863da6590f3e
IainT52 <iaintier@buffalo.edu>
2021-10-04 14:01:04 -0400

    Initial files and gitignore - simple canvas drawing capabilities added.

---
+++ canvas.js
@@ -0,0 +1,45 @@
+// Init Canvas State
+var canvas = $("#drawing-board")
+var ctx = canvas[0].getContext('2d')
+var offset = canvas.offset()
+var curPos = {x: 0, y: 0}
+var prevPos = {x: 0, y: 0}
+var clicking = false;
+resize()
+
+
+// Event listeners
+canvas.mousedown(e => {setPosition(e), clicking = !clicking})
+canvas.mousemove(draw)
+canvas.mouseup(e => clicking = !clicking)
+$( window ).resize(resize)
+
+
+function setPosition(e) {
+    curPos.x = e.clientX - offset.left
+    curPos.y = e.clientY - offset.top
+    return
+}
+
+
+function draw(e) {
+    if (clicking) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = '#c0392b';
+
+        ctx.moveTo(curPos.x, curPos.y);
+        setPosition(e);
+        ctx.lineTo(curPos.x, curPos.y);
+
+        ctx.stroke();
+    }
+}
+
+
+function resize() {
+    ctx.canvas.width = canvas.width()
+    ctx.canvas.height = canvas.height()
+    offset = canvas.offset()
+}

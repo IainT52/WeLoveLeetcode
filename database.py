7b740cca2daa10c488577120b155d0fbd5ce0932
Jean <jean.vigroux@gmail.com>
2021-12-05 21:48:07 -0500

    A

--- database.py
+++ database.py
@@ -3,19 +3,31 @@ from auth import encrypt
 
 tokens = set()
 
+# database = mysql.connector.connect(
+#     # host="mysql",
+#     user="root",
+#     password="vigroux18",
+#     database="canvas"
+# )
+
+
 database = mysql.connector.connect(
     # host="mysql",
     user="root",
-    password="cse312homework",
+    password="vigroux18",
     database="canvas"
 )
 # iain db 312db pass 1F900b8b3d52;
 # nick pass cse312homework
 mycursor = database.cursor(prepared=True)
 
-mycursor.execute("CREATE TABLE IF NOT EXISTS registration (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE (username))")
-mycursor.execute("CREATE TABLE IF NOT EXISTS cookie_jar (id INT AUTO_INCREMENT PRIMARY KEY, token VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, UNIQUE (username))")
-mycursor.execute("CREATE TABLE IF NOT EXISTS gallery (id INT AUTO_INCREMENT PRIMARY, username VACHAR(255) NOT NULL, image BLOB)")
+mycursor.execute(
+    "CREATE TABLE IF NOT EXISTS registration (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE (username))")
+mycursor.execute(
+    "CREATE TABLE IF NOT EXISTS cookie_jar (id INT AUTO_INCREMENT PRIMARY KEY, token VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, UNIQUE (username))")
+mycursor.execute(
+    "CREATE TABLE IF NOT EXISTS gallery (id INT AUTO_INCREMENT PRIMARY KEY, username VACHAR(255) NOT NULL, image BLOB)")
+
 
 def paint(username, image):
     command = "SELECT * FROM registration WHERE username = (?)"
@@ -24,12 +36,13 @@ def paint(username, image):
     if fetch == []:
         return False
     command = "INSERT INTO gallery (username, image) VALUES (?,?)"
-    value = (username, image, )
+    value = (username, image,)
     mycursor.execute(command, value)
     database.commit()
     return True
 
-#Register function returns a boolean - if true, username registered, if false, username not registered.
+
+# Register function returns a boolean - if true, username registered, if false, username not registered.
 def register(username, password):
     hashed = encrypt.encrypt(password)
     if hashed:
@@ -38,12 +51,13 @@ def register(username, password):
         fetch = mycursor.fetchall()
         if fetch == []:
             command = "INSERT INTO registration (username, password) VALUES (?, ?)"
-            value = (username, hashed, )
+            value = (username, hashed,)
             mycursor.execute(command, value)
             database.commit()
             return True
     return False
-    
+
+
 def login(username, password):
     command = "SELECT * FROM registration WHERE username = (?)"
     mycursor.execute(command, (username,))
@@ -53,22 +67,24 @@ def login(username, password):
     hashed = fetch[0][2]
     return encrypt.verify(password, hashed)
 
+
 def tokenize(token, username):
     command = "SELECT * FROM cookie_jar WHERE username = (?)"
     mycursor.execute(command, (username,))
     fetch = mycursor.fetchall()
     if fetch == []:
         command = "INSERT INTO cookie_jar (token, username) VALUES (?,?)"
-        value = (token, username, )
+        value = (token, username,)
         mycursor.execute(command, value)
         database.commit()
         print("Token created!")
     else:
         command = "UPDATE cookie_jar SET token = (?) WHERE username = (?)"
-        values = (token, username, )
+        values = (token, username,)
         mycursor.execute(command, values)
         database.commit()
         print("Token updated!")
+
 
 def get_token(username):
     command = "SELECT * FROM cookie_jar WHERE username = (?)"
@@ -77,6 +93,7 @@ def get_token(username):
     if fetch == []:
         return ''
     return fetch[0][1]
+
 
 def get_user(token):
     command = "SELECT * FROM cookie_jar WHERE token = (?)"
